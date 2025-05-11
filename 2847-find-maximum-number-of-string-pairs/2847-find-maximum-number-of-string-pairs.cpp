7093c0a70a51d715ef6ebe16877de8c5634f6ce8
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-05-12 00:33:55 +0530

    Time: 0 ms (100%), Space: 25.5 MB (69.92%) - LeetHub

--- 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.cpp
@@ -6,29 +6,32 @@ public:
         int n = words.size();
 
 
-        for(int i=0; i<n-1; i++){
+        // for(int i=0; i<n-1; i++){
 
-            string rev = words[i];
-            reverse(rev.begin(), rev.end());
-            for(int j=i+1; j<n; j++){
-                if(rev == words[j]){
-                    count++;
-                }
-            }
-        }
-        return count;
-
-        // unordered_set<string> s;
-
-        // for(string st : words){
-        //     s.insert(st);
-        //     string str = reverse(st);
-        //     if(s.find(str) != s.end()){
-        //         count++;
+        //     string rev = words[i];
+        //     reverse(rev.begin(), rev.end());
+        //     for(int j=i+1; j<n; j++){
+        //         if(rev == words[j]){
+        //             count++;
+        //         }
         //     }
         // }
+        // return count;
 
-        // return count;      
+        unordered_set<string> s;
+
+        for(string st : words){
+            s.insert(st);
+            string str = st;
+            reverse(str.begin(),str.end());
+            if(st == str) continue;
+            if(s.find(str) != s.end()){
+                s.erase(str);
+                count++;
+            }
+        }
+
+        return count;      
         
     }
 };
