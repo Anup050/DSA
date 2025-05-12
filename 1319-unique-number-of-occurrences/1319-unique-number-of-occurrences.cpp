8761757b2738d4670a109f833f2d95801248a574
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-05-12 10:28:10 +0530

    Time: 0 ms (100%), Space: 11.9 MB (57.23%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+
+        unordered_map<int,int> mp;
+        unordered_set<int> s;
+
+        for(int ele : arr){
+            mp[ele]++;
+        }
+
+        for(auto i : mp){
+            s.insert(i.second);
+        }
+
+        return mp.size()==s.size();
+    }
+};
