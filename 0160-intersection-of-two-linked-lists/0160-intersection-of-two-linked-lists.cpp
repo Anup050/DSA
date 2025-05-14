6d062acbf5e9ddab062c7c3847448164c1b74c93
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-05-14 17:14:54 +0530

    Time: 24 ms (98.95%), Space: 18.4 MB (82.12%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,22 +9,64 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        //O(n+m):time, O(n):space
+
+        // ListNode *t1 = headA;
+        // ListNode *t2 = headB;
+        // ListNode *temp = NULL;
+        // unordered_set<ListNode*> s;
+
+        // while(t1){
+        //     s.insert(t1);
+        //     t1 = t1->next;
+        // }
+        // while(t2){
+        //     if(s.find(t2) != s.end()){
+        //         temp = t2;
+        //         return temp;
+        //     }
+        //     t2 = t2->next;
+        // }
+        // return temp;
+
+        //O(n+m):time, O(1):space
+
         ListNode *t1 = headA;
         ListNode *t2 = headB;
-        ListNode *temp = NULL;
-        unordered_set<ListNode*> s;
 
+        int lenA = 0;
         while(t1){
-            s.insert(t1);
+            lenA++;
             t1 = t1->next;
         }
+        int lenB = 0;
         while(t2){
-            if(s.find(t2) != s.end()){
-                temp = t2;
-                return temp;
-            }
+            lenB++;
             t2 = t2->next;
         }
-        return temp;
+
+        t1 = headA;
+        t2 = headB;
+        if(lenA > lenB){
+            int diff = lenA - lenB;
+            for(int i=1; i<=diff; i++){
+                t1 = t1->next;
+            }
+            while(t1 != t2){
+                t1 = t1->next;
+                t2 = t2->next;
+            }
+            return t1;
+        }else{
+            int diff = lenB - lenA;
+            for(int i=1; i<=diff; i++){
+                t2 = t2->next;
+            }
+            while(t1 != t2){
+                t1 = t1->next;
+                t2 = t2->next;
+            }
+            return t2;
+        }
     }
 };
