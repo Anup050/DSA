c7fdd17ee20689fff14669b0b13e110af164dfb1
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-05-18 13:42:00 +0530

    Time: 0 ms (100%), Space: 19.4 MB (62%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -19,14 +19,14 @@ public:
             if(list1->val < list2->val){
                 temp->next = list1;
                 list1 = list1->next;
-                temp = temp->next;
+                
             }else{
                 temp->next = list2;
                 list2 = list2->next;
-                temp = temp->next;
             }
-            
+            temp = temp->next;
         }
+        
         if(list1 == NULL){
             temp->next = list2;
         }else{
