2bfa772f93596c2572237d84ec84bed93c4559f6
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-05-20 23:28:46 +0530

    Time: 0 ms (100%), Space: 13.5 MB (39.29%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,6 +11,7 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+
         ListNode* curr = head;
         ListNode* nn = NULL;
         ListNode* prev = NULL;
@@ -20,6 +21,7 @@ public:
             prev = curr;
             curr = nn;
         }
+        
         return prev;
     }
 };
