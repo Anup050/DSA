963e0fd49ca39775f72de3c6a9a163f70422359b
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-05-21 15:42:09 +0530

    Time: 71 ms (5.13%), Space: 152.5 MB (5.21%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,19 +10,54 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        vector<int> v;
-    
-        ListNode* temp = head;
-        while(temp){
-            v.push_back(temp->val);
-            temp = temp->next;
+    ListNode* reverse(ListNode* head) {
+
+        ListNode* curr = head;
+        ListNode* nn = NULL;
+        ListNode* prev = NULL;
+        while(curr){
+            nn = curr->next;
+            curr->next=prev;
+            prev = curr;
+            curr = nn;
         }
-        int i=0,j=v.size()-1;
-        while(i<j){
-            if(v[i] != v[j]) return false;
-            i++;
-            j--;
+        
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        // vector<int> v;
+    
+        // ListNode* temp = head;
+        // while(temp){
+        //     v.push_back(temp->val);
+        //     temp = temp->next;
+        // }
+        // int i=0,j=v.size()-1;
+        // while(i<j){
+        //     if(v[i] != v[j]) return false;
+        //     i++;
+        //     j--;
+        // }
+        // return true;
+
+        ListNode* temp  = head;
+        ListNode* curr  = new ListNode(0);
+        ListNode* dummy = curr;
+    
+        while(temp){
+            dummy->next = new ListNode(temp->val);
+            dummy = dummy->next;
+            temp  = temp->next; 
+        }
+
+        curr = curr->next;
+        curr = reverse(curr);
+        ListNode* a = head;
+        ListNode* b = curr;
+        while(a){
+            if(a->val != b->val) return false;
+            a = a->next;
+            b = b->next;
         }
         return true;
     }
