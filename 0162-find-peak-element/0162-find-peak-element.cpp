ea37e0c675207a146baa2063bb5818ca01607e40
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-05-26 23:57:06 +0530

    Time: 0 ms (100%), Space: 12.5 MB (66.74%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -3,12 +3,25 @@ public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
         
-        for (int i = 0; i < n; i++) {
-            if ((i == 0 || nums[i] > nums[i - 1]) && (i == n - 1 || nums[i] > nums[i + 1])) {
-                return i;
+        //O(n) : Time
+        // for (int i = 0; i < n; i++) {
+        //     if ((i == 0 || nums[i] > nums[i - 1]) && (i == n - 1 || nums[i] > nums[i + 1])) {
+        //         return i;
+        //     }
+        // }
+        
+        //O(log n) : Time
+       int l = 0, h = nums.size() - 1;
+       while (l < h) {
+            int mid = l + (h - l) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                h = mid;
+            } else {
+                l = mid + 1;
             }
         }
-        
-        return -1;
+
+        return l;
     }
 };
