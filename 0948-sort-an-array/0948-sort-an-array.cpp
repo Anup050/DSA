22b29c7cd16360bd2b22cf254b9ec722d1783f51
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-05-30 09:51:16 +0530

    Time: 1366 ms (10.6%), Space: 445.7 MB (5.02%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,7 +1,50 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums;
+        // sort(nums.begin(),nums.end());
+        // return nums;
+
+        if (nums.size() <= 1) return nums;
+
+        int mid = nums.size() / 2;
+
+        // Create left and right subarrays
+        vector<int> left(nums.begin(), nums.begin() + mid);
+        vector<int> right(nums.begin() + mid, nums.end());
+
+        // Recursive sort
+        left = sortArray(left);
+        right = sortArray(right);
+
+        return merge(left, right);
+    }
+
+private:
+    vector<int> merge(const vector<int>& first, const vector<int>& second) {
+        int i = 0, j = 0;
+        vector<int> mix;
+
+        while (i < first.size() && j < second.size()) {
+            if (first[i] < second[j]) {
+                mix.push_back(first[i]);
+                i++;
+            } else {
+                mix.push_back(second[j]);
+                j++;
+            }
+        }
+
+        // Copy remaining elements
+        while (i < first.size()) {
+            mix.push_back(first[i]);
+            i++;
+        }
+
+        while (j < second.size()) {
+            mix.push_back(second[j]);
+            j++;
+        }
+
+        return mix;
     }
 };
