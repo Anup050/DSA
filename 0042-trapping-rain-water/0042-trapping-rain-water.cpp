63d4afe0e8715db14e34c6bc0f9f53194eb178e0
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-06-02 15:11:35 +0530

    Time: 0 ms (100%), Space: 26 MB (60.25%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
     int trap(vector<int>& h) {
-        int n=h.size();
-        vector<int> pr(n);
-        vector<int> rr(n);
-        pr[0]=h[0];
-        rr[n-1]=h[n-1];
-        for(int i=1;i<n;i++){
-            pr[i]=max(pr[i-1],h[i]);
+        // int n=h.size();
+        // vector<int> pr(n);
+        // vector<int> rr(n);
+        // pr[0]=h[0];
+        // rr[n-1]=h[n-1];
+        // for(int i=1;i<n;i++){
+        //     pr[i]=max(pr[i-1],h[i]);
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     rr[i]=max(rr[i+1],h[i]);
+        // }
+        // int water=0;
+        // for(int i=0;i<n;i++){
+        //     int ans=min(pr[i],rr[i]);
+        //     if(ans-h[i]>0) water+=ans-h[i];
+        // }
+        // return water;
+        int left = 0, right = h.size() - 1;
+        int left_max = 0, right_max = 0;
+        int water = 0;
+
+        while (left <= right) {
+            if (h[left] <= h[right]) {
+                if (h[left] >= left_max) {
+                    left_max = h[left];
+                } else {
+                    water += left_max - h[left];
+                }
+                left++;
+            } else {
+                if (h[right] >= right_max) {
+                    right_max = h[right];
+                } else {
+                    water += right_max - h[right];
+                }
+                right--;
+            }
         }
-        for(int i=n-2;i>=0;i--){
-            rr[i]=max(rr[i+1],h[i]);
-        }
-        int water=0;
-        for(int i=0;i<n;i++){
-            int ans=min(pr[i],rr[i]);
-            if(ans-h[i]>0) water+=ans-h[i];
-        }
+
         return water;
     }
 };
