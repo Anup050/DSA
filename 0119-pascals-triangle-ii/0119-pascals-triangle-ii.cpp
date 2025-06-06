c83bbe02c15dee825c196994e0480221791de054
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-06-07 00:28:24 +0530

    Time: 0 ms (100%), Space: 8.9 MB (75.7%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -16,7 +16,7 @@ public:
         // vector<vector<int>> v;
 
         // for(int i=0; i<=n; i++){
-        //     vector<int> row(i);
+        //     vector<int> row(i+1);
         //     v.push_back(row);
         //     for(int j=0; j<=i; j++){
         //         if(j == 0 || j == i){
