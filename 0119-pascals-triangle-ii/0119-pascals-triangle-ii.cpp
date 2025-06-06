1a2101d746b180e4a21aa796b9457c79d88f9cff
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-06-06 23:52:40 +0530

    Time: 0 ms (100%), Space: 9 MB (54.24%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        int n = rowIndex;
-        vector<vector<int>> v;
-        for(int i=1; i<=n+1; i++){
-            vector<int> row(i);
-            v.push_back(row);
-        }
 
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=i; j++){
-                if(j == 0 || j == i){
-                    v[i][j] = 1;
-                }else{
-                    v[i][j] = v[i-1][j] + v[i-1][j-1];
-                }
+        vector<int> row(rowIndex + 1, 0);
+        row[0] = 1;
+
+        for(int i = 1; i <= rowIndex; i++) {
+            for(int j = i; j >= 1; j--) {
+                row[j] += row[j - 1];
             }
         }
-        vector<int> ans(n+1);
-        for(int i=0; i<=n; i++){
-            ans[i] = v[n][i];
-        }
-        return ans;
+
+        return row;
+        // int n = rowIndex;
+        // vector<vector<int>> v;
+        // for(int i=1; i<=n+1; i++){
+        //     vector<int> row(i);
+        //     v.push_back(row);
+        // }
+
+        // for(int i=0; i<=n; i++){
+        //     for(int j=0; j<=i; j++){
+        //         if(j == 0 || j == i){
+        //             v[i][j] = 1;
+        //         }else{
+        //             v[i][j] = v[i-1][j] + v[i-1][j-1];
+        //         }
+        //     }
+        // }
+        // vector<int> ans(n+1);
+        // for(int i=0; i<=n; i++){
+        //     ans[i] = v[n][i];
+        // }
+        // return ans;
     }
 };
