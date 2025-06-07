457b07e2ae91ce6ad636dc2cdcc6ff947f2c87bd
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-06-07 19:31:43 +0530

    Time: 0 ms (100%), Space: 13.2 MB (75.71%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,10 +4,26 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(matrix[i][j] == target){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j] == target){
-                    return true;
+            if(matrix[i][0] <= target && target <= matrix[i][n-1]){
+                int start=0,end=n-1;
+                while(start<=end){
+                    int mid = start + (end-start)/2;
+                    if(matrix[i][mid] == target){
+                        return true;
+                    }else if(matrix[i][mid] > target){
+                        end = mid-1;
+                    }else{
+                        start = mid+1;
+                    }
                 }
             }
         }
