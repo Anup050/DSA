da2cecbd7d0c2d7819add566385d143641aaef25
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-06-17 14:03:27 +0530

    Time: 9 ms (51.71%), Space: 19.7 MB (9.34%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -2,24 +2,34 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
 
-        map<int,int> mp;
-        int index = 0;
+        // map<int,int> mp;
+        // int index = 0;
 
-        for(int i : nums){
-            mp[i]++;
-        }
+        // for(int i : nums){
+        //     mp[i]++;
+        // }
 
-        for(auto i : mp){
-            if(i.second < 3){
-                for(int j = 1; j<=i.second; j++){
-                    nums[index++] = i.first;
-                }
-            }else{
-                for(int j = 1; j<=2; j++){
-                    nums[index++] = i.first;
-                }
+        // for(auto i : mp){
+        //     if(i.second < 3){
+        //         for(int j = 1; j<=i.second; j++){
+        //             nums[index++] = i.first;
+        //         }
+        //     }else{
+        //         for(int j = 1; j<=2; j++){
+        //             nums[index++] = i.first;
+        //         }
+        //     }
+        // }
+        // return index;
+
+        int n = nums.size();
+        if(n <= 2) return n;
+        int i=2;
+        for(int j=2; j<n; j++){
+            if(nums[j] != nums[i-2]){
+                nums[i++] = nums[j];
             }
         }
-        return index;
+        return i;
     }
 };
