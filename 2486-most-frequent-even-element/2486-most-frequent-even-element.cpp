5d994aa394f716a19215eb19eae3175f662c7498
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-06-21 16:37:57 +0530

    Time: 24 ms (62.83%), Space: 41.8 MB (81.78%) - LeetHub

---
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        int m = INT_MAX, fq = 0;
+        for(int num : nums){
+            if(num%2==0){
+                mp[num]++;
+            }
+        }
+        for(auto i : mp){
+            if(i.second>fq){
+                fq= i.second;
+                m = i.first;
+            }
+            if(i.second == fq && i.first<m){
+                m = i.first;
+            }
+        }
+        return (m == INT_MAX) ? -1 : m;
+    }
+};
