fe70aee588b3f8c44b8829401b4afd75ff269963
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-07-01 22:34:14 +0530

    Time: 4 ms (19.5%), Space: 28.2 MB (59.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
 
-        // TC : nlogn  SC : O(1)
-        sort(nums.begin(),nums.end());
-        return (nums[nums.size()/2]);
-        // unordered_map<int,int> mp;
+        // TC : O(nlogn)  SC : O(1)
+        // sort(nums.begin(),nums.end());
+        // return (nums[nums.size()/2]);
+
+        //TC : O(n)      SC : O(1)
+        unordered_map<int,int> mp;
         
-        // for(int num : nums){
-        //     mp[num]++;
-        // }
-        // for(auto i : mp){
-        //     if(i.second > nums.size()/2){
-        //         return i.first;
-        //     }
-        // }
-        // return -1;
+        for(int num : nums){
+            mp[num]++;
+        }
+        for(auto i : mp){
+            if(i.second > nums.size()/2){
+                return i.first;
+            }
+        }
+        return -1;
     }
 };
