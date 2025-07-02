b10f1fa4edba121aa2e6096c91b12d6e67ddc097
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-07-02 12:24:07 +0530

    Time: 387 ms (46.43%), Space: 185.8 MB (30.62%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,50 +1,39 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        // sort(nums.begin(),nums.end());
-        // return nums;
+    void merge(vector<int>& nums, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
 
-        if (nums.size() <= 1) return nums;
+        vector<int> L(n1), R(n2);
 
-        int mid = nums.size() / 2;
+        for (int i = 0; i < n1; i++) L[i] = nums[left + i];
+        for (int i = 0; i < n2; i++) R[i] = nums[mid + 1 + i];
 
-        // Create left and right subarrays
-        vector<int> left(nums.begin(), nums.begin() + mid);
-        vector<int> right(nums.begin() + mid, nums.end());
+        int i = 0, j = 0, k = left;
 
-        // Recursive sort
-        left = sortArray(left);
-        right = sortArray(right);
-
-        return merge(left, right);
-    }
-
-private:
-    vector<int> merge(const vector<int>& first, const vector<int>& second) {
-        int i = 0, j = 0;
-        vector<int> mix;
-
-        while (i < first.size() && j < second.size()) {
-            if (first[i] < second[j]) {
-                mix.push_back(first[i]);
-                i++;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                nums[k++] = L[i++];
             } else {
-                mix.push_back(second[j]);
-                j++;
+                nums[k++] = R[j++];
             }
         }
 
-        // Copy remaining elements
-        while (i < first.size()) {
-            mix.push_back(first[i]);
-            i++;
-        }
+        while (i < n1) nums[k++] = L[i++];
+        while (j < n2) nums[k++] = R[j++];
+    }
 
-        while (j < second.size()) {
-            mix.push_back(second[j]);
-            j++;
+    void mergeSort(vector<int>& nums, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid + 1, right);
+            merge(nums, left, mid, right);
         }
+    }
 
-        return mix;
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
     }
 };
