e5edafa3231200046a65e42bd81be8a15d12bbf2
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-07-13 02:45:00 +0530

    Time: 0 ms (100%), Space: 12.2 MB (64.4%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -3,11 +3,12 @@ public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
         sort(citations.begin(), citations.end());
-        int i;
-        for(i=1; i<=n; i++){
+        int i=1;
+        while(i<=n){
             if(citations[n-i]<i){
                 break;
             }
+            i++;
         }
         return i-1;
     }
