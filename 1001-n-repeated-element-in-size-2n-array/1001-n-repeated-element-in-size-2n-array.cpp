b1b69c923b7303acf2e460ca44d1b981899049d1
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-07-16 22:37:34 +0530

    Time: 19 ms (29.05%), Space: 34.1 MB (19.43%) - LeetHub

---
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i : nums){
+            mp[i]++;
+        }
+        for(auto i : mp){
+            if(i.second == nums.size()/2){
+                return i.first;
+            }
+        }
+        return -1;
+    }
+};
