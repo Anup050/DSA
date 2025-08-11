9d77d5b8420575ad5e31aae0b33b6502248d0789
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-08-11 20:33:27 +0530

    Time: 3 ms (13.41%), Space: 14 MB (80.73%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -4,7 +4,7 @@ public:
         int n = prices.size();
         for(int i=0; i<n-1; i++){
             for(int j=i+1; j<n; j++){
-                if(j>i && prices[j] <= prices[i]){
+                if(prices[j] <= prices[i]){
                     prices[i] -= prices[j];
                     break;
                 }
