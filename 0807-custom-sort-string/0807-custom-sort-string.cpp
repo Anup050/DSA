dc998bf39ee3fdd7ac2837ad44c09593501e8bbd
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-08-30 02:12:37 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.06%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.cpp
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
+        // unordered_map<char,int> mp;
+        // string ans = "";
+        // for(char c : s){
+        //     mp[c]++;
+        // }
+        // for(char ch : order){
+        //     if(mp.find(ch) != mp.end()){
+        //         while(mp[ch] > 0){
+        //             ans += ch;
+        //             mp[ch]--;
+        //         }
+        //     }
+        // }
+        // for(auto& i : mp){
+        //     while(i.second > 0){
+        //         ans += i.first;
+        //         mp[i.first]--;
+        //     }
+        // }
+        // return ans;
+
         unordered_map<char,int> mp;
-        string ans = "";
-        for(char c : s){
-            mp[c]++;
+        for(int i=0; i<order.length(); i++){
+            mp[order[i]] = i;
         }
-        for(char ch : order){
-            if(mp.find(ch) != mp.end()){
-                while(mp[ch] > 0){
-                    ans += ch;
-                    mp[ch]--;
-                }
-            }
-        }
-        for(auto& i : mp){
-            while(i.second > 0){
-                ans += i.first;
-                mp[i.first]--;
-            }
-        }
-        return ans;
+        sort(s.begin(), s.end(), [&mp] (char ch1, char ch2){
+            return mp[ch1] < mp[ch2];
+        });
+        return s;
     }
 };
