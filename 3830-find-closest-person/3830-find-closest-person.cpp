b3940134d660b97b1b947432659842f439af82f0
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-09-04 20:45:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (21.23%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.cpp
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
-        int count1 = 0, count2 = 0;
-        if(x<z){
-            // while(x != z){
-            //     x++;
-            //     count1++;
-            // }
-            count1 = z-x;
-        }else if(x > z){
-            // while(x != z){
-            //     x--;
-            //     count1++;
-            // }
-            count1 = x-z;
-        }
 
-         if(y<z){
-            // while(y != z){
-            //     y++;
-            //     count2++;
-            // }
-            count2 = z-y;
-        }else if(y > z){
-            // while(y != z){
-            //     y--;
-            //     count2++;
-            // }
-            count2 = y-z;
-        }
-        return (count1 < count2) ? 1 : (count2 < count1) ? 2 : 0;
+
+        return (abs(z-x) < abs(z-y)) ? 1 : (abs(z-x) > abs(z-y)) ? 2 : 0;
+        //int count1 = 0, count2 = 0;
+        // if(x<z){
+        //     // while(x != z){
+        //     //     x++;
+        //     //     count1++;
+        //     // }
+        //     count1 = z-x;
+        // }else if(x > z){
+        //     // while(x != z){
+        //     //     x--;
+        //     //     count1++;
+        //     // }
+        //     count1 = x-z;
+        // }
+
+        //  if(y<z){
+        //     // while(y != z){
+        //     //     y++;
+        //     //     count2++;
+        //     // }
+        //     count2 = z-y;
+        // }else if(y > z){
+        //     // while(y != z){
+        //     //     y--;
+        //     //     count2++;
+        //     // }
+        //     count2 = y-z;
+        // }
+        //return (count1 < count2) ? 1 : (count2 < count1) ? 2 : 0;
     }
 };
