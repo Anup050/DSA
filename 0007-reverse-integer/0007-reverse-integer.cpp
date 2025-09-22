8ae8864a0bb292747dbb73419f906e0ee72d94d5
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-09-22 20:37:52 +0530

    Time: 0 ms (100%), Space: 8.5 MB (53.68%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -6,8 +6,8 @@ public:
         while (x != 0) {
             int digit = x % 10;
 
-            // Check for overflow before multiplying by 10
-            if (reverse > INT_MAX / 10 || reverse < INT_MIN / 10) return 0;
+            if (reverse > INT_MAX / 10 || (reverse == INT_MAX / 10 && digit > 7)) return 0;
+            if (reverse < INT_MIN / 10 || (reverse == INT_MIN / 10 && digit < -8)) return 0;
 
             reverse = (reverse * 10) + digit;
             x /= 10;
