c3c0fb27b9c945749595409418205c4ce8073754
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-09-23 18:59:02 +0530

    Time: 0 ms (100%), Space: 10.4 MB (18.47%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
+    bool ans(string &str, int start, int end){
+        if(start>=end) return true;
+        if(str[start] != str[end]) return false;
+        return ans(str, start+1, end-1);
+    }
     bool isPalindrome(string s) {
+        if(s.empty()) return true;
         string str = "";
         for(char c : s){
             if(isalnum(c)){
                 str += tolower(c);
             }
         }
-        int i=0,j=str.length()-1;
-        while(i<=j){
-            if(str[i++] != str[j--]) return false;
-        }
-        return true;
+        return ans(str, 0, str.length()-1);
     }
 };
