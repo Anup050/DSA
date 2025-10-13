25918ddc3c35a226e177aa484f6eba82843e0a07
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-10-13 23:58:48 +0530

    Time: 3 ms (48.84%), Space: 17.1 MB (29.19%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
+    bool isAnagram(string &s1, string &s2){
+        int arr[26] = {0};
+
+        for(char &c : s1){
+            arr[c - 'a']++;
+        }
+        for(char &c : s2){
+            arr[c - 'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(arr[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
     vector<string> removeAnagrams(vector<string>& words) {
+        // vector<string> str;
+        // str.push_back(words[0]);
+        // for(int i=1; i<words.size(); i++){
+        //     string s = words[i-1];
+        //     sort(s.begin(),s.end());
+        //     string t = words[i];
+        //     sort(t.begin(),t.end());
+        //     if(s != t){
+        //         str.push_back(words[i]);
+        //     }
+        // }
+        // return str;
         vector<string> str;
         str.push_back(words[0]);
         for(int i=1; i<words.size(); i++){
-            string s = words[i-1];
-            sort(s.begin(),s.end());
-            string t = words[i];
-            sort(t.begin(),t.end());
-            if(s != t){
+            if(!isAnagram(words[i],str.back())){
                 str.push_back(words[i]);
             }
         }
