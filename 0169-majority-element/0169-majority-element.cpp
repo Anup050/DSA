02c517ced4f663faa2610e38112ea058836dd87b
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-10-19 01:56:10 +0530

    Time: 0 ms (100%), Space: 28.3 MB (7.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int i : nums){
-            mp[i]++;
-        }
-        for(auto &i : mp){
-            if(i.second > nums.size()/2){
-                return i.first;
+        // unordered_map<int,int> mp;
+        // for(int i : nums){
+        //     mp[i]++;
+        // }
+        // for(auto &i : mp){
+        //     if(i.second > nums.size()/2){
+        //         return i.first;
+        //     }
+        // }
+        // return -1;
+        
+        //Moore's Voting Algo
+        int cnt = 0;
+        int ele = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(cnt == 0){
+                cnt = 1;
+                ele = nums[i];
+            }else if(nums[i] == ele){
+                cnt++;
+            }else{
+                cnt--;
             }
+        }
+        int cnt2 = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == ele) cnt2++;
+            if(cnt2 > nums.size()/2) return ele;
         }
         return -1;
     }
