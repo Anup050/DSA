272c7670e55f29a55ac4f6bdfc95a0c75e7a9a0f
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-10-19 02:14:39 +0530

    Time: 11 ms (32.44%), Space: 128.2 MB (78.94%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> v1(nums.size()/2);
-        vector<int> v2(nums.size()/2);
-        //vector<int> res(nums.size());
-        int j=0,k=0;
+
+        // 2 pass : O(N)time + O(n)extra space
+
+        // vector<int> v1(nums.size()/2);
+        // vector<int> v2(nums.size()/2);
+        // int j=0,k=0;
+        // for(int i : nums){
+        //     if(i>0){
+        //         v1[j++] = i;
+        //     }else{
+        //         v2[k++] = i;
+        //     }
+        // }
+        // int i=0;
+        // j=0,k=0;
+        // while(j<v1.size() && k<v2.size()){
+        //     nums[i++] = v1[j++];
+        //     nums[i++] = v2[k++];
+        // }
+        // return nums;
+
+        // 1 pass : O(N)time + O(n)extra space
+        vector<int> ans(nums.size());
+        int j = 0, k = 1;
         for(int i : nums){
             if(i>0){
-                v1[j++] = i;
+                ans[j] = i;
+                j += 2;
             }else{
-                v2[k++] = i;
+                ans[k] = i;
+                k += 2;
             }
         }
-        int i=0;
-        j=0,k=0;
-        while(j<v1.size() && k<v2.size()){
-            nums[i++] = v1[j++];
-            nums[i++] = v2[k++];
-        }
-        return nums;
+        return ans;
     }
 };
