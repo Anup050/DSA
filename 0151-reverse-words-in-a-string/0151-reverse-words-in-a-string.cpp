b2254941e3b44801d897aa8264034a2bdd7f05b6
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-10-19 19:10:34 +0530

    Time: 3 ms (40.15%), Space: 23.9 MB (8.56%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> st;
-        int i=0;
-        while(i<s.length()){
-            if(s[i] == ' ') i++;
-            else break;
+        stringstream ss(s);
+        string token = "";
+        string res = "";
+        while(ss >> token){
+            res = token + " "+ res;
         }
-        int j = s.length()-1;
-        while(j>0){
-            if(s[j] == ' ') j--;
-            else break;
-        }
-        string str = "";
-        bool flag = false;
-        while(i<=j){
-            if(s[i] != ' '){
-                str += s[i];
-                flag = true;
-            }else if(flag){
-                st.push_back(str);
-                st.push_back(" ");
-                str = "";
-                flag = false;
-            }
-            i++;
-        }
-        st.push_back(str);
-        s = "";
-        for(int i=0; i<st.size(); i++){
-            s = st[i] + s;
-        }
-        return s;
+        return res.substr(0,res.length()-1);
+        // vector<string> st;
+        // int i=0;
+        // while(i<s.length()){
+        //     if(s[i] == ' ') i++;
+        //     else break;
+        // }
+        // int j = s.length()-1;
+        // while(j>0){
+        //     if(s[j] == ' ') j--;
+        //     else break;
+        // }
+        // string str = "";
+        // bool flag = false;
+        // while(i<=j){
+        //     if(s[i] != ' '){
+        //         str += s[i];
+        //         flag = true;
+        //     }else if(flag){
+        //         st.push_back(str);
+        //         st.push_back(" ");
+        //         str = "";
+        //         flag = false;
+        //     }
+        //     i++;
+        // }
+        // st.push_back(str);
+        // s = "";
+        // for(int i=0; i<st.size(); i++){
+        //     s = st[i] + s;
+        // }
+        // return s;
     }
 };
