df2fbbfcccab5f14407ba55c691646969c5cdebc
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-11-01 01:43:32 +0530

    Time: 16 ms (60.97%), Space: 27.1 MB (26.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
+    string generate(string &word){
+        int arr[26] = {0};
+        for(char &ch : word){
+            arr[ch - 'a']++;
+        }
+        string ans = "";
+        for(int i=0; i<26; i++){
+            int freq = arr[i];
+            if(freq>0){
+                ans += string(freq, i+'a');
+            }
+        }
+        return ans;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        //(n*klogk)
         vector<vector<string>> ans;
         unordered_map<string,vector<string>> mp;
         for(int i=0; i<strs.size(); i++){
-            string temp = strs[i];
-            sort(begin(temp),end(temp));
+            //string temp = strs[i];
+            //sort(begin(temp),end(temp));
+            string temp = generate(strs[i]);
             mp[temp].push_back(strs[i]);
         }
         for(auto i : mp){
