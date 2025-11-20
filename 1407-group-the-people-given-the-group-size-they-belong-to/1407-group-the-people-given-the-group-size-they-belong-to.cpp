d1baf0c4391e91f4e12652b638d4305205ea0d7d
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-11-20 21:15:50 +0530

    Time: 6 ms (35.32%), Space: 16.9 MB (64.5%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -3,23 +3,32 @@ public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         vector<vector<int>> v;
         unordered_map<int,vector<int>> mp;
-        for(int i=0; i<groupSizes.size(); i++){
-            mp[groupSizes[i]].push_back(i);
-        }
-        for(auto &i : mp){
-           if(i.first == i.second.size()) v.push_back(i.second);
-           else{
-            int n = i.second.size();
-            int j=0;
-            while(j<n){
-                vector<int> ans;
-                int m = i.first;
-                while(m--){
-                    ans.push_back(i.second[j++]);
-                }
-                v.push_back(ans);
+        // for(int i=0; i<groupSizes.size(); i++){
+        //     mp[groupSizes[i]].push_back(i);
+        // }
+        // for(auto &i : mp){
+        //    if(i.first == i.second.size()) v.push_back(i.second);
+        //    else{
+        //     int n = i.second.size();
+        //     int j=0;
+        //     while(j<n){
+        //         vector<int> ans;
+        //         int m = i.first;
+        //         while(m--){
+        //             ans.push_back(i.second[j++]);
+        //         }
+        //         v.push_back(ans);
+        //     }
+        //    }
+        // }
+        for (int i = 0; i < groupSizes.size(); i++) {
+            int g = groupSizes[i];
+            mp[g].push_back(i);
+
+            if (mp[g].size() == g) {
+                v.push_back(mp[g]);
+                mp[g].clear();          
             }
-           }
         }
         return v; 
     }
