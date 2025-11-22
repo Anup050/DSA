10b7706a9a2d821d8e3457cc84e104b2aeb77657
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-11-23 01:42:49 +0530

    Time: 2 ms (17.14%), Space: 9.1 MB (5.36%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countPoints(string rings) {
+        unordered_map<int,unordered_set<char>> mp;
+        int count = 0;
+        for(int i=1; i<rings.length(); i+=2){
+            mp[rings[i]].insert(rings[i-1]);
+        }
+        for(auto i : mp){
+            if(i.second.size() == 3) count++;
+        }
+        return count;
+    }
+};
