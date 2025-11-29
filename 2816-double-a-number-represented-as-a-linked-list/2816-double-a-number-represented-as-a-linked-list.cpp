0024adc93becce6841567205f24515d0876fe314
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-11-29 19:13:33 +0530

    Time: 11 ms (34.66%), Space: 132 MB (33.98%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -11,37 +11,65 @@
 class Solution {
 public:
     ListNode* doubleIt(ListNode* head) {
-        stack<int> st1;
-        stack<int> st2;
-        ListNode* temp = head;
-        while(temp){
-            st1.push(temp->val);
-            temp = temp->next;
-        }
-        int carry = 0;
-        ListNode* d = new ListNode(0);
-        while(!st1.empty()){
-           int n = st1.top()*2 + carry;
-           st2.push(n%10);
-           carry = n/10;
-           st1.pop();
-           if(st1.size() == 0 && carry != 0){
-            d->val = carry;
-           }
-        }
-        if(d->val != 0){
-            d->next = head;
-            temp = head;
-            head = d;
-        }else{
-            temp = head;
-        }
+        // stack<int> st1;
+        // stack<int> st2;
+        // ListNode* temp = head;
+        // while(temp){
+        //     st1.push(temp->val);
+        //     temp = temp->next;
+        // }
+        // int carry = 0;
+        // ListNode* d = new ListNode(0);
+        // while(!st1.empty()){
+        //    int n = st1.top()*2 + carry;
+        //    st2.push(n%10);
+        //    carry = n/10;
+        //    st1.pop();
+        //    if(st1.size() == 0 && carry != 0){
+        //     d->val = carry;
+        //    }
+        // }
+        // if(d->val != 0){
+        //     d->next = head;
+        //     temp = head;
+        //     head = d;
+        // }else{
+        //     temp = head;
+        // }
 
-        while(!st2.empty()){
-            temp->val = st2.top();
-            st2.pop();
+        // while(!st2.empty()){
+        //     temp->val = st2.top();
+        //     st2.pop();
+        //     temp = temp->next;
+        // }
+        // return head;
+
+        stack<ListNode*> st;
+        ListNode* temp = head;
+        
+        while (temp) {
+            st.push(temp);
             temp = temp->next;
         }
+        
+        int carry = 0;
+        
+        while (!st.empty()) {
+            ListNode* node = st.top();
+            st.pop();
+            
+            int total = node->val * 2 + carry;
+            node->val = total % 10;
+            carry = total / 10;
+        }
+        
+        if (carry > 0) {
+            ListNode* newHead = new ListNode(carry);
+            newHead->next = head;
+            head = newHead;
+        }
+        
         return head;
+
     }
 };
