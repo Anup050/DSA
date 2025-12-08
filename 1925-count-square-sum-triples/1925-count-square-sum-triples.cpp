b4091dc1de6997d08dcafb38c66cf30fd8916fb3
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-12-08 18:25:22 +0530

    Time: 3 ms (90.49%), Space: 7.8 MB (84.51%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -3,10 +3,13 @@ public:
     int countTriples(int n) {
         int count = 0;
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                for(int k=1; k<=n; k++){
-                    if((i*i)+(j*j) == (k*k)) count++;
-                }
+            for(int j=i+1; j<=n; j++){
+                // for(int k=1; k<=n; k++){
+                //     if((i*i)+(j*j) == (k*k)) count++;
+                // }1
+                int k = (i*i) + (j*j);
+                int x = sqrt(k);
+                if(x*x == k && x<=n) count += 2;
             }
         }
         return count;
