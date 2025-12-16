fec337730839bf2af58dfa3b53d3b615586e7f1f
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-12-17 01:48:21 +0530

    Time: 4 ms (22.17%), Space: 63.1 MB (12.25%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,13 +2,6 @@ class Solution {
 public:
     int maxArea(vector<int>& h) {
         int mx = 0;
-        //TLE O(n^2):
-        // for(int i=0; i<h.size(); i++){
-        //     for(int j=1; j<h.size(); j++){
-        //         int ar = min(h[i],h[j])* (j-i);
-        //         mx = max(mx, ar);
-        //     }
-        // }
         int i=0,j=h.size()-1;
         while(j>i){
             int ar = min(h[i],h[j]) * (j-i);
