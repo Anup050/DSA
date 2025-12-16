2309dea7185a43db4b6dbce2517b738b4b825512
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-12-17 01:51:57 +0530

    Time: 0 ms (100%), Space: 23.8 MB (81.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& v) {
-        int i = 0;
-        for(int j = 0; j<v.size(); j++){
-            if(v[j] != 0){
-                swap(v[j], v[i]);
-                i++;
-            }
-        }
+         int idx = 0;
+         for(int i=0; i<v.size(); i++){
+            if(v[i] != 0) swap(v[idx++],v[i]);
+         }
     }
 };
