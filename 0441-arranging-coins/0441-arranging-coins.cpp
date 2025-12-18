47bf8d4e6b63a8733f126bd046004d65a9859ba3
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-12-18 19:22:08 +0530

    Time: 13 ms (6.88%), Space: 8.9 MB (33.86%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int ans = 1;
-        int i=1;
-        n--;
+        int ans = 0;
+        int i=0;
         while(n){
             ans = max(ans,i);
             if(n-i>=0) n -= i;
             else{
                 break;
             }
-            if(i<=n) i++;
+            if(i<n) i++;
         }
         return ans;
     }
