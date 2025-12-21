1154d8682069dc2c938e3e3f3a0dda39779a9b65
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2025-12-21 16:33:34 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.12%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int maximumScore(int a, int b, int c) {
-        priority_queue<int> pq;
-        pq.push(a);pq.push(b);pq.push(c);
-        int count = 0;
-        while(pq.size()>1){
-            int n1 = pq.top();
-            pq.pop();
-            int n2 = pq.top();
-            pq.pop();
-            count++;
-            n2--;n1--;
-            if(n1>0) pq.push(n1);
-            if(n2>0) pq.push(n2);
+
+        long long sum = 1LL*a + b + c;
+        long long mx = max({a, b, c});
+
+        if(mx > sum - mx) {
+            return sum - mx;
         }
 
-        return count;
+        return sum / 2;
+
+
+        // priority_queue<int> pq;
+        // pq.push(a);pq.push(b);pq.push(c);
+        // int count = 0;
+        // while(pq.size()>1){
+        //     int n1 = pq.top();
+        //     pq.pop();
+        //     int n2 = pq.top();
+        //     pq.pop();
+        //     count++;
+        //     n2--;n1--;
+        //     if(n1>0) pq.push(n1);
+        //     if(n2>0) pq.push(n2);
+        // }
+
+        // return count;
     }
 };
