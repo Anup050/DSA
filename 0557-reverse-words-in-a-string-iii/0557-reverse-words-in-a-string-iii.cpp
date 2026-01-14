ecc680de3ef96ba9894d66ac7a0b94e3abf6b65f
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2026-01-15 01:09:23 +0530

    Time: 4 ms (39.87%), Space: 15.9 MB (27.58%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     string reverseWords(string s) {
         string ans = "";
-        vector<string> v;
+        //vector<string> v;
         string word = "";
         for(char c : s){
             if(c != ' '){
                 word += c;
             }else{
                 reverse(begin(word),end(word));
-                v.push_back(word);
+                //v.push_back(word);
+                ans += " " + word;
                 word = "";
             }
         }
         reverse(begin(word),end(word));
-        v.push_back(word);
-        for(int i=0; i<v.size(); i++){
-            if(i != 0) ans += " ";
-            ans += v[i];
-        }
-        return ans;
+        //v.push_back(word);
+        ans += " " + word;
+        // for(int i=0; i<v.size(); i++){
+        //     if(i != 0) ans += " ";
+        //     ans += v[i];
+        // }
+        // return ans;
+        return ans.substr(1);
     }
 };
