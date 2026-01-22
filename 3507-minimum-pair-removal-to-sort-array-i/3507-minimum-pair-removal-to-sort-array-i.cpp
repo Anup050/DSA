4cef9bbc67e325f9d087ac0f9406a80bd3c39033
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2026-01-22 22:35:51 +0530

    Time: 0 ms (100%), Space: 26.4 MB (81.3%) - LeetHub

---
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumPairRemoval(vector<int>& nums) {
+        int count = 0;
+        bool flag  = true;
+        while(flag){
+            if(is_sorted(begin(nums),end(nums))) break;
+            int minPairSum = INT_MAX;
+            int idx = 0;
+            for(int i=1; i<nums.size(); i++){
+                int sum = nums[i] + nums[i-1];
+                if(sum < minPairSum){
+                    minPairSum = sum;
+                    idx = i-1;
+                }
+            }
+            nums[idx] = minPairSum;
+            nums.erase(begin(nums)+idx+1);
+            count++;
+        }
+        return count;
+    }
+};
