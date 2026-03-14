89e79864d1076c680bfa9ba9e1c8f1e6185c7677
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2026-03-15 00:24:09 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.74%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void solve(int n, string &curr, int &count, int k, string &result) {
+        if (curr.length() == n) {
+            count++;
+            if (count == k) {
+                result = curr;
+            }
+            return;
+        }
+        for (char ch = 'a'; ch <= 'c'; ch++) {
+            if (!curr.empty() && curr.back() == ch)
+                continue;
+            curr.push_back(ch);
+            solve(n, curr, count, k, result);
+            if (!result.empty()) return;
+            curr.pop_back();
+        }
+    }
+
+    string getHappyString(int n, int k) {
+        string curr = "";
+        string result = "";
+        int count = 0;
+        solve(n, curr, count, k, result);
+        return result;
+    }
+};
