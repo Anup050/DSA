ee8ca18b5d0f0ec1da25293f08387f4f54cdab45
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2026-04-01 15:50:29 +0530

    Time: 2 ms (86.09%), Space: 47.4 MB (19.66%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -11,20 +11,36 @@
 class Solution {
 public:
     vector<int> nextLargerNodes(ListNode* head) {
-        vector<int> ans;
-        ListNode* curr = head;
-        while(curr){
-            ListNode* temp = curr;
-            while(temp && temp->val <= curr->val){
-                temp = temp->next;
+        vector<int> arr;
+        while(head) {
+            arr.push_back(head->val);
+            head = head->next;
+        }
+        int n = arr.size();
+        vector<int> ans(n, 0);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[i] > arr[st.top()]) {
+                ans[st.top()] = arr[i];
+                st.pop();
             }
-            if(temp){
-                ans.push_back(temp->val);
-            }else{
-                ans.push_back(0);
-            }
-            curr = curr->next;
+            st.push(i);
         }
         return ans;
+        // vector<int> ans;
+        // ListNode* curr = head;
+        // while(curr){
+        //     ListNode* temp = curr;
+        //     while(temp && temp->val <= curr->val){
+        //         temp = temp->next;
+        //     }
+        //     if(temp){
+        //         ans.push_back(temp->val);
+        //     }else{
+        //         ans.push_back(0);
+        //     }
+        //     curr = curr->next;
+        // }
+        // return ans;
     }
 };
