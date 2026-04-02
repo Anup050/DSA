936e787a5980b8cce0a92fea8b9af82868535784
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2026-04-02 22:31:14 +0530

    Time: 8 ms (26.95%), Space: 124.3 MB (68.2%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -11,17 +11,36 @@
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        vector<int> v;
-        ListNode* temp = head;
-        while(temp){
-            v.push_back(temp->val);
-            temp = temp->next;
+        // vector<int> v;
+        // ListNode* temp = head;
+        // while(temp){
+        //     v.push_back(temp->val);
+        //     temp = temp->next;
+        // }
+        // int mSum = INT_MIN;
+        // for(int i=0; i<v.size(); i++){
+        //     int j = v.size()-1-i;
+        //     mSum = max(mSum,v[i]+v[j]);
+        // }
+        // return mSum;
+        ListNode *f = head, *s = head;
+        while(f && f->next){
+            s = s->next;
+            f = f->next->next;
         }
-        int mSum = INT_MIN;
-        for(int i=0; i<v.size(); i++){
-            int j = v.size()-1-i;
-            mSum = max(mSum,v[i]+v[j]);
+        ListNode *prev = NULL, *nn = NULL;
+        while(s){
+            nn = s->next;
+            s->next = prev;
+            prev = s;
+            s = nn;
         }
-        return mSum;
+        int maxS = INT_MIN;
+        while(prev){
+            maxS = max(maxS, prev->val + head->val);
+            prev = prev->next;
+            head = head->next;
+        }
+        return maxS;
     }
 };
