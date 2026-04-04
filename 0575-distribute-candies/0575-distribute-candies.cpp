0fe695f0530aa7e41aa4d15e6f42f9a0493ed095
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2026-04-04 23:53:10 +0530

    Time: 103 ms (64.4%), Space: 120.1 MB (62.1%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        int count = 0;
-        int n = candyType.size();
         unordered_set<int> s;
-        for(int i : candyType){
-            s.insert(i);
-        }
-        int  m = s.size();
-        return (m<n/2) ? m : n/2;
+        int n = candyType.size();
+        for(int i : candyType) s.insert(i);
+        return (s.size() >= n / 2) ? n/2 : s.size();
     }
 };
