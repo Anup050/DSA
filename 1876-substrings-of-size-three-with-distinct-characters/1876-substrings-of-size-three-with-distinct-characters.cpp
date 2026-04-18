ba8aca6cad36eb63bdf368861385a10e277c7351
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2026-04-18 15:38:34 +0530

    Time: 0 ms (100%), Space: 8.4 MB (72.31%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,16 +1,50 @@
 class Solution {
 public:
+    // bool check(vector<int> & v){
+    //     int count= 0;
+    //     for(int i=0; i<26; i++){
+    //         if(v[i] > 0) count++;
+    //     }
+    //     return count == 3;
+    // }
     int countGoodSubstrings(string s) {
-        if(s.length()<3) return 0;
+        if(s.length() < 3) return 0;
         int count = 0;
-        unordered_set<char> st;
-        for(int i=2; i<s.length();i++){
-            st.insert(s[i-2]);
-            st.insert(s[i-1]);
-            st.insert(s[i]);
-            if(st.size() == 3) count++;
-            st.clear();
+        for(int i = 0; i < s.length() - 2; i++) {
+            if(s[i] != s[i+1] && s[i] != s[i+2] && s[i+1] != s[i+2]) {
+                count++;
+            }
         }
         return count;
+
+
+
+        // if(s.length()<3) return 0;
+        // int count = 0;
+        // unordered_set<char> st;
+        // for(int i=2; i<s.length();i++){
+        //     st.insert(s[i-2]);
+        //     st.insert(s[i-1]);
+        //     st.insert(s[i]);
+        //     if(st.size() == 3) count++;
+        //     st.clear();
+        // }
+        // return count;
+
+
+
+        // if(s.length()< 3) return 0;
+        // int count = 0;
+        // vector<int> fq(26, 0);
+        // for(int i=0; i<3; i++){
+        //     fq[s[i] - 'a']++;
+        // }
+        // if(check(fq)) count++;
+        // for(int i=3; i<s.length(); i++){
+        //     fq[s[i-3] - 'a']--;
+        //     fq[s[i] - 'a']++;
+        //     if(check(fq)) count++;
+        // }
+        // return count;
     }
 };
