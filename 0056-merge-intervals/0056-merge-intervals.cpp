89e2490bb47e0f6e3779605190f7f2da97aacf1e
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2026-05-23 07:36:03 +0530

    Time: 3 ms (89.54%), Space: 23.9 MB (59.21%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,21 +3,22 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(begin(intervals), end(intervals));
         vector<vector<int>> ans;
-        int minE = intervals[0][0];
-        int maxE = intervals[0][1];
-        for(int i=1; i<intervals.size(); i++){
-            int a = intervals[i][0];
-            int b = intervals[i][1];
-            if(maxE >= a){
-                minE = min(minE, a);
-                maxE = max(maxE, b);
-            }else{
-                ans.push_back({minE, maxE});
-                minE = a;
-                maxE = b;
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for(int i = 1; i < intervals.size(); i++) {
+            int currStart = intervals[i][0];
+            int currEnd = intervals[i][1];
+            if(end >= currStart) {
+                end = max(end, currEnd);
+            }
+            else {
+                ans.push_back({start, end});
+                start = currStart;
+                end = currEnd;
             }
         }
-        ans.push_back({minE, maxE});
+        ans.push_back({start, end});
         return ans;
     }
 };
