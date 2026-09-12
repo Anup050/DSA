28c476153b4b1590e5ef56eb9df79a54afc3fb90
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2026-09-12 20:28:16 +0530

    Time: 0 ms (100%), Space: 20.3 MB (5.53%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,18 +4,32 @@ public:
         unordered_map<int,int> mp;
         vector<int> v;
         for(int num : nums) mp[num]++;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        // priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        // for(auto i : mp){
+        //     pq.push({i.second,i.first});
+        //     if(pq.size() > k) pq.pop();
+        // }
+        // while(k){
+        //     pair<int,int> p = pq.top();
+        //     v.push_back(p.second);
+        //     pq.pop();
+        //     k--;
+        // }
+        vector<vector<int>> ans(nums.size()+1);
         for(auto i : mp){
-            pq.push({i.second,i.first});
-            if(pq.size() > k) pq.pop();
+            ans[i.second].push_back(i.first);
         }
-        while(k){
-            pair<int,int> p = pq.top();
-            v.push_back(p.second);
-            pq.pop();
-            k--;
+        for(int i=ans.size()-1; i>=0; i--){
+            vector<int> vt = ans[i];
+            int n = vt.size();
+            while(n){
+                v.push_back(vt[--n]);
+            }
+            if(v.size() == k) break;
         }
         return v;
+
+
         
     }
 };
