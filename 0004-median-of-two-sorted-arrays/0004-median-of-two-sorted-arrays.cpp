39cbf9645d41ad6599728b2e00db6be58dbe7293
Anup Kumar Dubey <dubeyanupkumar349@gmail.com>
2026-09-15 01:31:06 +0530

    Time: 0 ms (100%), Space: 95.1 MB (60.93%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() > nums2.size())
-            return findMedianSortedArrays(nums2, nums1);
-
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-
-        int low = 0, high = n1;
-
-        while(low <= high){
-
-            int cut1 = (low + high) / 2;
-            int cut2 = (n1 + n2 + 1) / 2 - cut1;
-
-            int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
-            int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
-
-            int r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
-            int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
-
-            if(l1 <= r2 && l2 <= r1){
-
-                if((n1 + n2) % 2 == 0){
-                    return (max(l1,l2) + min(r1,r2)) / 2.0;
-                }
-                return max(l1,l2);
-            }else if(l1 > r2){
-                high = cut1 - 1;
+        int n = nums1.size();
+        int m = nums2.size();
+        int count = 0;
+        int idx1 = (n+m)/2;
+        int idx2 = idx1-1;
+        double ele1 = 0;
+        double ele2 = 0;
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(nums1[i]<nums2[j]){
+                if(idx1 == count) ele1 = nums1[i];
+                if(idx2 == count) ele2 = nums1[i];
+                i++;
             }else{
-                low = cut1 + 1;
+                if(idx1 == count) ele1 = nums2[j];
+                if(idx2 == count) ele2 = nums2[j];
+                j++;
             }
+            count++;
         }
-        return 0.0;
+        while(i<n){
+            if(idx1 == count) ele1 = nums1[i];
+            if(idx2 == count) ele2 = nums1[i];
+            count++;
+            i++;
+        }
+        while(j<m){
+            if(idx1 == count) ele1 = nums2[j];
+            if(idx2 == count) ele2 = nums2[j];
+            count++;
+            j++;
+        }
+        return ((n+m)%2==0) ? (ele1+ele2)/2.0 : ele1;
     }
 };
